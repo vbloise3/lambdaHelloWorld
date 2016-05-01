907501387a8c306a8e2c44a23fd4c4484aa29586
Vince Bloise <vbloise3@verizon.net>
2016-05-01 13:45:15 -0400

    Added some additional logging to the hello world lambda

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
  * Created by vincebloise on 4/29/16.
  */
 exports.handler = function(event, context) {
+    console.log(" Request ID: " + context.awsRequestId);
+    console.log(" Log Group Name: " + context.logGroupName);
+    console.log(" Log Stream Name: " + context.logStreamName);
+    console.log(" Identity: " + context.identity);
+    console.log(" Function Name: " + context.functionName);
     context.succeed(" Hello, " + event.username);
 };
