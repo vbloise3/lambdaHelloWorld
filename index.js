4b065beb6380fccea7061b9daac1c2f38cc836ba
Vince Bloise <vbloise3@verizon.net>
2016-05-03 18:16:49 -0400

    Changed to AWS Lambda in Action hello world lambda

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
 /**
  * Created by vincebloise on 4/29/16.
  */
-var aws = require('aws-sdk');
-var s3 = new aws.S3();
-exports.handler = function( event, context) {
-// Get the object from the event and show its content type
-var bucket = event.Records[ 0]. s3. bucket.name;
-    var key = decodeURIComponent(
-        event.Records[ 0].s3.object.key.replace(/\+/g, ''));
-    var params = {
-        Bucket: bucket,
-        Key: key
-    };
-    s3.getObject( params, function( err, data) {
-        if (err) {
-            console.log( err);
-            context.fail(' Error getting object ' +
-                key + ' from bucket ' + bucket +
-                '. Make sure they exist and your ' +
-                'bucket is in the same region as ' +
-                'this function.');
-        } else {
-            context.succeed(' Hello, ' + data.Body);
-        }
-    });
+console.log('loading function');
+exports.handler = function(event, context) {
+    console.log('Received event', JSON.stringify(event, null, 2));
+    console.log('name =', event.name);
+    var name = '';
+    if ('name' in event) {
+        name = event['name'];
+    } else {
+        name = 'World';
+    }
+    var greetings = 'Hello ' + name + '!';
+    console.log(greetings);
+    context.succeed(greetings);
 };
 
