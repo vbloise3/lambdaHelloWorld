c42a7263aaede613f3bf461353c477f996b04a3f
Vince Bloise <vbloise3@verizon.net>
2016-05-02 21:28:13 -0400

    Added s3 integration to the hello world lambda

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 /**
  * Created by vincebloise on 4/29/16.
  */
-exports.handler = function(event, context) {
-    console.log(" Request ID: " + context.awsRequestId);
-    console.log(" Log Group Name: " + context.logGroupName);
-    console.log(" Log Stream Name: " + context.logStreamName);
-    console.log(" Identity: " + context.identity);
-    console.log(" Function Name: " + context.functionName);
-    context.succeed(" Hello, " + event.username);
+var aws = require('aws-sdk');
+var s3 = new aws.S3();
+exports.handler = function( event, context) {
+// Get the object from the event and show its content type
+var bucket = event.Records[ 0]. s3. bucket.name;
+    var key = decodeURIComponent(
+        event.Records[ 0].s3.object.key.replace(/\+/g, ''));
+    var params = {
+        Bucket: bucket,
+        Key: key
+    };
+    s3.getObject( params, function( err, data) {
+        if (err) {
+            console.log( err);
+            context.fail(' Error getting object ' +
+                key + ' from bucket ' + bucket +
+                '. Make sure they exist and your ' +
+                'bucket is in the same region as ' +
+                'this function.');
+        } else {
+            context.succeed(' Hello, ' + data.Body);
+        }
+    });
 };
+
