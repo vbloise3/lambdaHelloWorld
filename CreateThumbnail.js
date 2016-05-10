0183f5cc3649d2de0b3ac03c92e902851780b2ac
Vince Bloise <vbloise3@verizon.net>
2016-05-09 21:07:00 -0400

    Tries to fix security failure in username to greetings lambda function

--- CreateThumbnail.js
+++ CreateThumbnail.js
@@ -5,7 +5,7 @@
 var async = require('async');
 var AWS = require('aws-sdk');
 var gm = require('gm')
-    .subClass({ imageMagick: true }); // Enable ImageMagick integration.
+    .subClass({ imageMagick: true }); // Enable ImageMagick image translation integration.
 var util = require('util');
 
 // constants
