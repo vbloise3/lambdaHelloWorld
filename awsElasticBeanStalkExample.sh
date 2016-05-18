e58d5c3dc5eda777346ae1092127d2694aa5ace0
Vince Bloise <vbloise3@verizon.net>
2016-05-17 21:25:27 -0400

    Modified elasticbeanstalk script to terminate web app environment and destroy the ec2

--- awsElasticBeanStalkExample.sh
+++ awsElasticBeanStalkExample.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
-
+##to launch
 aws elasticbeanstalk create-application --application-name etherpad
 aws elasticbeanstalk create-application-version --application-name etherpad --version-label 1.5.2 --source-bundle S3Bucket=awsinaction,S3Key=chapter5/etherpad.zip
 aws elasticbeanstalk list-available-solution-stacks --output text --query "SolutionStacks[?contains(@, 'running Node.js')] | [0]"
 aws elasticbeanstalk create-environment --environment-name etherpad --application-name etherpad --option-settings Namespace=aws:elasticbeanstalk:environment,OptionName=EnvironmentType,Value=SingleInstance --solution-stack-name "64bit Amazon Linux 2016.03 v2.1.1 running Node.js" --version-label 1.5.2
 aws elasticbeanstalk describe-environments --environment-names etherpad
+
+##to terminate and destroy
+aws elasticbeanstalk terminate-environment --environment-name etherpad
+aws elasticbeanstalk describe-environments --environment-names etherpad
+##once status changes to Terminated
+aws elasticbeanstalk delete-application --application-name etherpad
